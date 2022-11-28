447e25703404771e4ca4cd9e26be23f2d4676189
Edleron <Edleron@hotmail.com>
2022-11-29 01:14:59 +0300

    comp : day of 15

---
+++ 30DaysOfCode/C#/DayOf-13/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace DayOf13
+{
+    abstract class Book
+    {
+
+        protected String title;
+        protected String author;
+
+        public Book(String t, String a)
+        {
+            title = t;
+            author = a;
+        }
+        public abstract void display();
+    }
+
+    class MyBook : Book
+    {
+        private int price = 0;
+        public MyBook(String newT, String newA, int newP) : base(newT, newA)
+        {
+            this.price = newP;
+        }
+        public override void display()
+        {
+            Console.Write("Title: {0} \nAuthor: {1} \nPrice: {2}", title, author, price);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String title = Console.ReadLine();
+            String author = Console.ReadLine();
+            int price = Int32.Parse(Console.ReadLine());
+            Book new_novel = new MyBook(title, author, price);
+            new_novel.display();
+        }
+    }
+}
---
+++ 30DaysOfCode/C#/DayOf-15/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace DayOf15
+{
+    class Node
+    {
+        public int index;
+        public int data;
+        public Node pointer;
+        public Node(int i, int d)
+        {
+            index = i;
+            data = d;
+            pointer = null;
+        }
+    }
+
+    class Program
+    {
+        public static Node insert(Node head, int index, int data)
+        {
+            if (head == null)
+            {
+                // Eğer dizide eleman yok ise burası çalışır. ilk eleman Head için çalışır.
+                head = new Node(index, data);
+                Console.WriteLine(" HEAD : index " + head.index + " ||| data " + head.data);
+            }
+            else
+            {
+                // Artık Head o anki currentent'dır.
+                Node current = head;
+                while (current.pointer != null)
+                {
+                    current = current.pointer;
+                }
+                current.pointer = new Node(index, data);
+
+                // Burada bir önceki pointer datasını, yeni yaratılan datayı ve index'i işaretledirk. işaretledik.             
+                Console.WriteLine(" Current Index : " + current.pointer.index + ", data : " + current.pointer.data + " ||| " + "pointer index : " + current.index + " ,pointer data : " + current.data);
+            }
+            return head;
+        }
+
+        public static void display(Node head)
+        {
+            Node start = head;
+            while (start != null)
+            {
+                Console.Write(start.data + " ");
+                start = start.pointer;
+            }
+        }
+        static void Main(String[] args)
+        {
+            Node head = null;
+            int T = int.Parse(Console.ReadLine());
+            int index = 0;
+            while (T-- > 0)
+            {
+                int data = int.Parse(Console.ReadLine());
+                head = insert(head, index, data);
+                index++;
+            }
+            display(head);
+        }
+    }
+}
+
+
