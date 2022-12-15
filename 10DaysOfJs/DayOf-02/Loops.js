1c0fe68ad0d9b869072581a7e94de787ffc8d360
Edleron <Edleron@hotmail.com>
2022-12-15 22:57:10 +0300

    comp : Loops

---
+++ 10DaysOfJs/DayOf-02/Loops.js
@@ -0,0 +1,56 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the vowelsAndConsonants function.
+ * Print your output using 'console.log()'.
+ */
+
+
+function vowelsAndConsonants(s) {
+    const vowels = 'aeiou';
+
+    for (let index = 0; index < s.length; index++) {
+        if (vowels.includes(s[index])) {
+            console.log(s[index]);
+        }
+    }
+
+    for (let index = 0; index < s.length; index++) {
+        if (!vowels.includes(s[index])) {
+            console.log(s[index]);
+        }
+    }
+}
+
+
+
+function main(input) {
+    //const s = readLine();
+    const s = input;
+    vowelsAndConsonants(s);
+}
+
+let values = "javascriptloops";
+main(values);
