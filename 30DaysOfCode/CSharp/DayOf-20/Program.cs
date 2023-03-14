8d278dc7056c35bf50b78567969a90837a33a8cc
Edleron <Edleron@hotmail.com>
2023-03-14 12:54:35 +0300

    comp : dayof 20 csharp

---
+++ 30DaysOfCode/CSharp/DayOf-20/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+namespace DayOf19
+{
+    class Solution
+    {
+        public static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
+
+            // Write your code here
+            int swaps = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n - 1; j++)
+                {
+                    if (a[j] > a[j + 1])
+                    {
+                        int v = a[j];
+                        a[j] = a[j + 1];
+                        a[j + 1] = v;
+                        swaps++;
+                    }
+                }
+
+                if (swaps == 0)
+                {
+                    break;
+                }
+            }
+            Console.WriteLine("Array is sorted in " + swaps + " swaps.");
+            Console.WriteLine("First Element: " + a[0]);
+            Console.WriteLine("Last Element: " + a[a.Count - 1]);
+        }
+    }
+}
