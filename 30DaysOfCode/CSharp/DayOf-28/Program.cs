e57a14cdf8dfcfe57b5a3f9e2250a62e8b9954f9
Edleron <Edleron@hotmail.com>
2023-04-18 15:03:44 +0300

    comp : day of 28

---
+++ 30DaysOfCode/CSharp/DayOf-28/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+namespace DayOf27
+{
+    class Solution
+    {
+        public static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine().Trim());
+
+            var checkList = new List<string>();
+
+            for (int NItr = 0; NItr < N; NItr++)
+            {
+                string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+                string firstName = firstMultipleInput[0];
+
+                string emailID = firstMultipleInput[1];
+
+                if (Regex.IsMatch(emailID, @".+@gmail\.com$"))
+                {
+                    checkList.Add(firstName);
+                }
+            }
+
+            checkList.Sort();
+
+            foreach (var name in checkList)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
